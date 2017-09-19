958dab3f1713427ad9ffdc200a5a865359a35e1b
Mleon195 <mleon@mail.ccsf.edu>
2017-09-19 13:35:23 -0700

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,12 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyAH-lTGqtYTmGNUkPFYOFlutFx8r2vDQfA",
+  authDomain: "businesscontacts-39e25.firebaseapp.com",
+  databaseURL: "https://businesscontacts-39e25.firebaseio.com",
+  projectId: "businesscontacts-39e25",
+  storageBucket: "businesscontacts-39e25.appspot.com"
+  }
 };
