8a89a6a659a21f7edd5d72877fe0d10e24c84da0
Mleon195 <mleon@mail.ccsf.edu>
2017-09-28 14:17:03 -0700

    fix edit function and add delete function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,11 +16,23 @@ export class AppComponent implements OnInit{
   appState: string;
   activeKey: string;
 
+  activeCompany: string;
+  activeCategory: string;
+  activeYearsInBusiness: string;
+  activeDescription: string;
+  activePhone: string;
+  activeEmail: string;
+  activeStreetAddress: string;
+  activeCity: string;
+  activeState: string;
+  activeZipcode: string;
+
   constructor(private _firebaseService: FirebaseService) {
 
  }
 
 ngOnInit(){
+  this.appState = "default";
   this._firebaseService.getBusinesses().subscribe(businesses => {
     this.businesses = businesses;
   });
@@ -70,9 +82,53 @@ ngOnInit(){
           created_at: created_at
         }
 
-        console.log(newBusiness);
+          console.log(newBusiness);
+
         this._firebaseService.addBusiness(newBusiness);
 
         //this.changeState('default');
+        //this.appState = state
+        this.appState = "default";
   }
+
+    showEdit(business){
+      this.changeState('edit', business.$key);
+      this.activeCompany =               business.company;
+      this.activeCategory =              business.category;
+      this.activeYearsInBusiness =       business.years_in_business;
+      this.activeDescription =           business.description;
+      this.activePhone =                 business.phone;
+      this.activeEmail =                 business.email;
+      this.activeStreetAddress =         business.street_address;
+      this.activeCity =                  business.city;
+      this.activeState =                 business.state;
+      this.activeZipcode =               business.zipcode;
+    }
+
+    updateBusiness(){
+        var updBusiness = {
+        company: this.activeCompany,
+        category: this.activeCategory,
+        years_in_business: this.activeYearsInBusiness,
+        description: this.activeDescription,
+        phone: this.activePhone,
+        email: this.activeEmail,
+        street_address: this.activeStreetAddress,
+        city: this.activeCity,
+        state: this.activeState,
+        zipcode: this.activeZipcode
+      }
+
+      this._firebaseService.updateBusiness(this.activeKey, updBusiness );
+
+      //this.changeState('state');
+      //this.appState = state;
+      this.appState = "default";
+    }
+
+    deleteBusiness(key){
+      this._firebaseService.deleteBusiness(key);
+
+      this.appState = "default";
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+
+
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
@@ -12,6 +15,7 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
   ],
